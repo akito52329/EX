3894cdca480d8e7e9da70c65afe73d285b9306b0
sakuseimoto@gmail.com <sakuseimoto@gmail.com>
2022-04-19 17:16:25 +0900

    new

---
+++ EX04/GetVolumeSurface.cpp
@@ -0,0 +1,14 @@
+#include "GetVolumeSurface.h"
+double GetVolumeSurface(double x, double y, double z, double& surface)
+{
+	double a;
+	double b;
+	double c;
+
+	a = 2 * x * y;
+	b = 2 * y * z;
+	c = 2 * x * z;
+	surface = a + b + c;
+
+	return x * y * z;
+}

42c6ce193177de494893639005278f701b3c8ced
sakuseimoto@gmail.com <sakuseimoto@gmail.com>
2022-04-20 11:48:33 +0900

    new

--- EX03/EX03.cpp
+++ EX03/EX03.cpp
@@ -4,13 +4,32 @@
 #include "GetObesit.h"
 using namespace std;
 
+string Obesit(int drees)
+{
+	switch (drees)
+	{
+	case -1:
+		return "低体重(痩せ型)";
+	case 0:
+		return "普通体重";
+	case 1:
+		return  "肥満(1度)";
+	case 2:
+		return  "肥満(2度)";
+	case 3:
+		return "肥満(3度)";
+	case 4:
+		return "肥満(4度)";
+	}
+}
+
 int main()
 {
 	double height;//身長
 	double weight;//体重
 	double bmi;//BMI
 	int obesitDrees;//肥満度
-
+	string obesit;//肥満度文字
 	cout << "身長(㎝)で入力してください：";
 	cin >> height;
 
@@ -19,30 +38,8 @@ int main()
 
 	bmi = GetBmi(height, weight);
 	obesitDrees = GetObesit(bmi);
-
-	string obesit;
-
-	switch (obesitDrees)
-	{
-	case -1:
-		obesit = "低体重(痩せ型)";
-		break;
-	case 0:
-		obesit = "普通体重";
-	case 1:
-		obesit = "肥満(1度)";
-		break;
-	case 2:
-		obesit = "肥満(2度)";
-		break;
-	case 3:
-		obesit = "肥満(3度)";
-		break;
-	case 4:
-		obesit = "肥満(4度)";
-		break;
-	}
-
+	obesit = Obesit(obesitDrees);
+	
 	cout << "あなたのBMIは" << fixed << setprecision(1) << bmi << "です" << endl;
 	cout << "肥満度は" << obesit << "です。" << endl;
 	return 0;

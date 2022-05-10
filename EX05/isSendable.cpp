41225e2de0d0c6d161ed36803daa416f6919aac1
sakuseimoto@gmail.com <sakuseimoto@gmail.com>
2022-05-10 15:19:58 +0900

    new

--- EX05/isSendable.cpp
+++ EX05/isSendable.cpp
@@ -5,43 +5,17 @@ bool isSendable(BOX* box)
 	const int MAX_COUNT = 8;
 	int sizeCeiteria[] = { 200,180,160,140,120,100,80,60 };//ƒTƒCƒY‚ÌŠî€
 	int size;
+	int overSize = 201;
 	size = box->x + box->y + box->z;
 
-	if (size <= sizeCeiteria[0])
+	box->packSize = overSize;
+	for (int i = 0; i < MAX_COUNT; i++)
 	{
-		box->packSize = sizeCeiteria[0];
-	}
-	else if (size <= sizeCeiteria[1])
-	{
-		box->packSize = sizeCeiteria[1];
-	}	
-	else if (size <= sizeCeiteria[2])
-	{
-		box->packSize = sizeCeiteria[2];
-	}	
-	else if (size <= sizeCeiteria[3])
-	{
-		box->packSize = sizeCeiteria[3];
-	}
-	else if (size <= sizeCeiteria[4])
-	{
-		box->packSize = sizeCeiteria[4];
-	}
-	else if (size <= sizeCeiteria[5])
-	{
-		box->packSize = sizeCeiteria[5];
-	}
-	else if (size <= sizeCeiteria[6])
-	{
-		box->packSize = sizeCeiteria[6];
-	}
-	else if (size <= sizeCeiteria[7])
-	{
-		box->packSize = sizeCeiteria[7];
-	}
-	else
-	{
-		box->packSize = 201;
+		
+		if (size <= sizeCeiteria[i])
+		{
+			box->packSize = sizeCeiteria[i];
+		}
 	}
 
 	return box->packSize <= sizeCeiteria[0];

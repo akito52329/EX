34d572ad631bb131ed0f200c0a3e49d2df4c5276
sakuseimoto@gmail.com <sakuseimoto@gmail.com>
2022-05-17 17:20:34 +0900

    ex07

---
+++ EX07/EX07.cpp
@@ -0,0 +1,187 @@
+
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+
+/// <summary>
+/// 物体の定義
+/// </summary>
+class Solid
+{
+public:
+	virtual double GetVolume() = 0;
+	virtual double GetSurface() = 0;
+};
+
+class Box : Solid
+{
+private:
+	double width;
+	double height;
+	double depth;
+
+public:
+	/// <summary>
+	/// Boxのコンストラクタ
+	/// </summary>
+	/// <param name="width">幅</param>
+	/// <param name="height">高さ</param>
+	/// <param name="depth">奥行</param>
+	Box(double width, double height, double depth)
+	{
+		this->width = width;
+		this->height = height;
+		this->depth = depth;
+	}
+
+	double GetVolume()
+	{
+		return width * height * depth;
+	}
+
+	double GetSurface()
+	{
+		return (width * height + height * depth + width * depth) * 2;
+	}
+
+};
+
+class Cylinder : Solid
+{
+private:
+	double radius;
+	double height;
+
+public:
+	/// <summary>
+	/// 円柱のコンストラクタ
+	/// </summary>
+	/// <param name="radius">半径</param>
+	/// <param name="height">高さ</param>
+	Cylinder(double radius, double height)
+	{
+		this->radius = radius;
+		this->height = height;
+	}
+
+	double GetVolume()
+	{
+		return radius * radius * M_PI * height;
+	}
+
+	double GetSurface()
+	{
+		return (radius * radius * M_PI * 2) + (radius * 2 * M_PI * height);
+	}
+
+};
+
+class Cone : Solid
+{
+private:
+	double radius;
+	double height;
+
+public:
+	/// <summary>
+	/// 円錐のコンストラクタ
+	/// </summary>
+	/// <param name="radius">半径</param>
+	/// <param name="height">高さ</param>
+	Cone(double radius, double height)
+	{
+		this->radius = radius;
+		this->height = height;
+	}
+
+	double GetVolume()
+	{
+		return radius * radius * M_PI * height / 3;
+	}
+
+	double GetSurface()
+	{
+		return (radius * M_PI) * (sqrt(radius * radius + height * height) * radius);
+	}
+
+};
+
+class Sphere : Solid
+{
+private:
+	double radius;
+
+public:
+	/// <summary>
+	/// 球のコンストラクタ
+	/// </summary>
+	/// <param name="radius">半径</param>
+	Sphere(double radius)
+	{
+		this->radius = radius;
+	}
+
+	double GetVolume()
+	{
+		return radius * radius * radius * M_PI * 4 / 3;
+	}
+
+	double GetSurface()
+	{
+		return radius * radius * M_PI * 4;
+	}
+
+};
+
+class Package
+{
+private:
+	double width;
+	double height;
+	double depth;
+
+public:
+
+	Package(double width, double height, double depth)
+	{
+		this->width = width;
+		this->height = height;
+		this->depth = depth;
+	}
+
+	double Size()
+	{
+		const int MAX_COUNT = 8;
+		int sizeCeiteria[] = { 200,180,160,140,120,100,80,60 };//サイズの基準
+		int size;
+		int packSize;
+		int overSize = 201;
+		size = width + height + depth;
+
+		packSize = overSize;
+		for (int i = 0; i < MAX_COUNT; i++)
+		{
+
+			if (size <= sizeCeiteria[i])
+			{
+				packSize = sizeCeiteria[i];
+			}
+		}
+		return packSize;
+	}
+};
+
+
+int main()
+{
+	Box box(0.5, 5, 2);
+	Cylinder cylinder(2, 2);
+	Cone cone(2, 3);
+	Sphere sphere(2);
+
+	cout << "Box　体積:" << box.GetVolume() << "表面積:" << box.GetSurface() << endl;
+	cout << "円柱　体積:" << cylinder.GetVolume() << "表面積:" << cylinder.GetSurface() << endl;
+	cout << "円錐　体積:" << cone.GetVolume() << "表面積:" << cone.GetSurface() << endl;
+	cout << "球　体積:" << sphere.GetVolume() << "表面積:" << sphere.GetSurface() << endl;
+}
